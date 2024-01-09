5334c1090387b06bc84a16b7d92bd3d95981f2ac
Julian Holfeld <julian@uni-kassel.de>
2024-01-09 17:49:35 +0100

    feat: simulated annealing

---
+++ exercise/05-simulated-annealing.py
@@ -0,0 +1,16 @@
+import random
+import math
+
+
+def accept_prob(S_old, S_new, T):
+    if S_new > S_old:
+        return 1.0
+    else:
+        return math.exp((S_new - S_old) / T)
+
+
+def accept(S_old, S_new, T):
+    if random.random() < accept_prob(S_old, S_new, T):
+        print(True)
+    else:
+        print(False)
